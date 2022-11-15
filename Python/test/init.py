80c0362aa61664bdcd80b97a5aded52afebc1ecc
HHgzs <3166237657@qq.com>
2022-11-15 17:26:35 +0800

    Python notebook

--- Python/test/init.py
+++ Python/test/init.py
@@ -3,6 +3,7 @@ texts = "Python"
 print(texts)
 #   int     long    float   complex
 #   bo
+
 a = "abc"
 b = 123.3
 b = int(b)
@@ -11,26 +12,32 @@ print(a)
 print(b)
 d = str(b)
 print(b)
+
 print(bool(''))
 print(bool(""))
 print(bool(0))
 print(bool({}))
 print(bool([]))
 print(bool(()))
+
 print(d * 3)
+
 print("number is %d %d, word is %s" % (b, c, a))
+
 if b == 123:
     print("Yes")
 elif b > 123:
     print("elif")
 else:
     print("No")
+
 for s in "Hello":
     print(s)
 for i in range(5):
     print(i)
 for i in range(3, 12, 5):
     print(i)
+
 str1 = "test class string in Python"
 print(str1.find('c'))
 
@@ -54,6 +61,48 @@ if findWord2 not in str2:
     print("No")
 else:
     print("Yes")
+
+print(str2)
 del str2[7]
 print(str2)
 str2.pop
+
+tuple = ('hello', 1024, 3.14)
+print(tuple[0])
+
+print(str2[0:4:2])
+
+info = {'name': 'HHgzs', 'age': '18', 'school': 'NULL'}
+print(info)
+info['school'] = 'TJU'
+print(info)
+info['hobby'] = 'photography'
+print(info)
+del info['hobby']
+print(info)
+
+for i in info.keys():
+    print(i)
+for i in info.values():
+    print(i)
+for i in info.items():
+    print(i)
+for i, j in info.items():
+    print("key = %s, value = %s" % (i, j))
+
+info.clear()
+
+
+def function1():
+    print("This is a function")
+    print("HERE")
+
+
+def function2(a, b):
+    c = a + b
+    return c
+
+
+print(function2(1, 1))
+
+f = open('test.txt', '')
