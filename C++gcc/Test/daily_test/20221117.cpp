2f9a744eacad972a8c69d0b1ce5cdf3d1eca653b
HHgzs <3166237657@qq.com>
2022-11-18 18:18:14 +0800

    11.18 18:18

--- C++gcc/Test/daily_test/20221117.cpp
+++ C++gcc/Test/daily_test/20221117.cpp
@@ -29,27 +29,29 @@
 //     }
 // }
 
-// #include <iostream>
-// #include <string.h>
-// using namespace std;
-// char *str_reverse(const char *str)
-// {
-//     char *char1 = new char(strlen(str));
-//     for (int i = 0; i < strlen(str); i++)
-//     {
-//         *(char1 + i) = str[strlen(str) - i - 1];
-//         //cout << *(char1 + i);
-//     }
-//     return char1;
-// }
+#include <iostream>
+#include <string.h>
+using namespace std;
+char *str_reverse(const char *str)
+{
+    char *char1 = new char(strlen(str));
+    for (int i = 0; i < strlen(str); i++)
+    {
+        *(char1 + i) = str[strlen(str) - i - 1];
+        //cout << *(char1 + i);
+    }
+    char *p=char1;
+    delete char1;
+    return p;
+}
 
-// int main()
-// {
-//     char str[100];
-//     cin >> str;
-//     cout << str_reverse(str);
-//     return 0;
-// }
+int main()
+{
+    char str[100];
+    cin >> str;
+    cout << str_reverse(str);
+    return 0;
+}
 
 // #include <windows.h>
 // #include <iostream>
@@ -142,129 +144,155 @@
 //     return 0;
 // }
 
-#include <windows.h>
-#include <iostream>
-#include <string>
-using namespace std;
+// #include <windows.h>
+// #include <iostream>
+// #include <string>
+// using namespace std;
 
-int main()
-{
-    int basic, num;
-    cin >> basic;
-    num = basic;
-    int a1 = num / 1000;
-    num = num % 1000;
-    int a2 = num / 500;
-    num = num % 500;
-    int a3 = num / 100;
-    num = num % 100;
-    int a4 = num / 50;
-    num = num % 50;
-    int a5 = num / 10;
-    num = num % 10;
-    int a6 = num / 5;
-    num = num % 5;
-    int a7 = num;
+// int main()
+// {
+//     int basic, num;
+//     cin >> basic;
+//     num = basic;
+//     int a1 = num / 1000;
+//     num = num % 1000;
+//     int a2 = num / 500;
+//     num = num % 500;
+//     int a3 = num / 100;
+//     num = num % 100;
+//     int a4 = num / 50;
+//     num = num % 50;
+//     int a5 = num / 10;
+//     num = num % 10;
+//     int a6 = num / 5;
+//     num = num % 5;
+//     int a7 = num;
 
-    for (int i = 0; i <= a1; i++)
-    {
-        if (a1 == 0 && a2 == 1 && a3 == 4)
-        {
-            cout << "CM";
-            a2 = 0;
-            a3 = 0;
-        }
-        else if (a1 > 0)
-            cout << 'M'; // 1000
-        a1--;
-    }
+//     for (int i = 0; i <= a1; i++)
+//     {
+//         if (a1 == 0 && a2 == 1 && a3 == 4)
+//         {
+//             cout << "CM";
+//             a2 = 0;
+//             a3 = 0;
+//         }
+//         else if (a1 > 0)
+//             cout << 'M'; // 1000
+//         a1--;
+//     }
 
-    for (int i = 0; i <= a2; i++)
-    {
-        if (a2 == 0 && a3 == 4)
-        {
-            cout << "CD";
-            a3 = 0;
-        }
-        else if (a2 > 0)
-            cout << 'D'; // 500
-        a2--;
-    }
+//     for (int i = 0; i <= a2; i++)
+//     {
+//         if (a2 == 0 && a3 == 4)
+//         {
+//             cout << "CD";
+//             a3 = 0;
+//         }
+//         else if (a2 > 0)
+//             cout << 'D'; // 500
+//         a2--;
+//     }
 
-    for (int i = 0; i <= a3; i++)
-    {
-        if (a3 == 0 && a4 == 1 && a5 == 4)
-        {
-            cout << "XC";
-            a4 = 0;
-            a5 = 0;
-        }
-        else if (a3 > 0)
-            cout << 'C'; // 100
-        a3--;
-    }
+//     for (int i = 0; i <= a3; i++)
+//     {
+//         if (a3 == 0 && a4 == 1 && a5 == 4)
+//         {
+//             cout << "XC";
+//             a4 = 0;
+//             a5 = 0;
+//         }
+//         else if (a3 > 0)
+//             cout << 'C'; // 100
+//         a3--;
+//     }
 
-    for (int i = 0; i <= a4; i++)
-    {
-        if (a4 == 0 && a5 == 4)
-        {
-            cout << "XL";
-            a5 = 0;
-        }
-        else if (a4 > 0)
-            cout << 'L'; // 50
-            a4--;
-    }
+//     for (int i = 0; i <= a4; i++)
+//     {
+//         if (a4 == 0 && a5 == 4)
+//         {
+//             cout << "XL";
+//             a5 = 0;
+//         }
+//         else if (a4 > 0)
+//             cout << 'L'; // 50
+//             a4--;
+//     }
 
-    for (int i = 0; i < a5; i++)
-    {
-        if (a5 == 0 && a7 == 4 && a6 == 1)
-        {
-            cout << "IX";
-            a6 = 0;
-            a7 = 0;
-        }
-        else if (a5 > 0)
-            cout << 'X'; // 10
-            a5--;
-    }
+//     for (int i = 0; i < a5; i++)
+//     {
+//         if (a5 == 0 && a7 == 4 && a6 == 1)
+//         {
+//             cout << "IX";
+//             a6 = 0;
+//             a7 = 0;
+//         }
+//         else if (a5 > 0)
+//             cout << 'X'; // 10
+//             a5--;
+//     }
 
-    for (int i = 0; i <= a6; i++)
-    {
-        if (a7 == 4 && a6 == 0)
-        {
-            cout << "IV";
-            a7 = 0;
-        }
-        else if (a6 > 0)
-            cout << 'V'; // 5
-            a6--;
-    }
+//     for (int i = 0; i <= a6; i++)
+//     {
+//         if (a7 == 4 && a6 == 0)
+//         {
+//             cout << "IV";
+//             a7 = 0;
+//         }
+//         else if (a6 > 0)
+//             cout << 'V'; // 5
+//             a6--;
+//     }
 
-    for (int i = 0; i < a7; i++)
-    {
-        cout << 'I'; // 1
-        a7--
-    }
-}
+//     for (int i = 0; i < a7; i++)
+//     {
+//         cout << 'I'; // 1
+//         a7--
+//     }
+// }
 
-#include <iostream>
-#include <string>
-using namespace std;
-int main()
-{
-    int num;
-    cin >> num;
-    int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};                   //数值数组，用于计算
-    string meps[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}; //罗马数字数组，用于映射
-    string result = "";
-    for (int i = 0; i < 13; i++)
-    {
-        while (num >= values[i]) //每次都用尽可能大的数字去显示
-        {
-            result += meps[i]; //链接字符
-            num -= values[i];  //减去已被映射过的数字
-        }
-    }
-    cout << result;
-}
+// #include <iostream>
+// #include <string>
+// using namespace std;
+// int main()
+// {
+//     int num;
+//     cin >> num;
+//     int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};                   //数值数组，用于计算
+//     string meps[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}; //罗马数字数组，用于映射
+//     string result = "";
+//     for (int i = 0; i < 13; i++)
+//     {
+//         while (num >= values[i]) //每次都用尽可能大的数字去显示
+//         {
+//             result += meps[i]; //链接字符
+//             num -= values[i];  //减去已被映射过的数字
+//         }
+//     }
+//     cout << result;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// char *cat_fun(const int* stra, char *strb)
+// {
+//     int *p;
+//     p = stra;
+//     // while (*stra != '\0')
+//     //     stra++;
+//     // while (*strb != '\0')
+//     // {
+//     //     *stra = *strb;
+//     //     stra++;
+//     //     strb++;
+//     // }
+//     // *stra = '\0';
+//     // return p;
+// }
+
+// int main()
+// {
+//     char stra[20], strb[20];
+//     cin >> stra >> strb;
+//     cout << cat_fun(stra, strb) << endl;
+// }
--- C++gcc/Test/daily_test/tempCodeRunnerFile.cpp
+++ C++gcc/Test/daily_test/tempCodeRunnerFile.cpp
@@ -1,1 +1,24 @@
 
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b = 1;
+    int c = 1;
+    cin >> a;
+    cout << "1" << endl;
+    if (a == 2)
+        cout << "1" << endl;
+    else if (a > 2)
+    {
+        cout << "1" << endl;
+        for (int i = 0; i < a - 2; i++)
+        {
+            int d = b + c;
+            b = c;
+            c = d;
+            cout << d << endl;
+        }
+    }
+}
