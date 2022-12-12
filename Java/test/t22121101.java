ace3ff0cde485c3cd0961cf6c0240bc1f2035843
HHgzs <3166237657@qq.com>
2022-12-12 09:43:13 +0800

    12.12 09:43

---
+++ Java/test/t22121101.java
@@ -0,0 +1,41 @@
+package test;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class t22121101 {
+    public static void main(String[] args) {
+    }
+    // Scanner sc = new Scanner(System.in);
+
+    // System.out.println("Enter : ");
+    // int i1 = sc.nextInt();
+    // int i2 = i1 / 10 % 10;
+    // System.out.println(i1 % 10);
+    // System.out.println(i2++);
+    // System.out.println(1 + 2 + "abc" + 2 + 1);
+
+    // int week = sc.nextInt();
+    // switch (week) {
+    // case 1, 2, 3, 4, 5 -> System.out.println("工作日");
+    // case 6, 7 -> System.out.println("休息日");
+    // default -> System.out.println("没有这个星期");
+    // }
+
+    // Random randomNum = new Random();
+    // int num = randomNum.nextInt(100);
+    // System.out.println(num);
+    // }
+
+    // public static void fn(int a) {
+    // }
+    // public static int fn(double a) {
+    //     return 1;
+    // }
+    // public static int fn(int a, int b) {
+    //     return 1;
+    // }
+
+
+
+}
